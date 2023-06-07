8e4d007310e7ec1ffe3e8f46b0d9699e2eeb751a
Dinith <codse202f060@student.nibm.lk>
2023-06-07 20:05:44 +0530

    first commit

---
+++ Course_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_AppApp.swift
+//  Course App
+//
+//  Created by Dinith Hasaranga on 2023-06-07.
+//
+
+import SwiftUI
+
+@main
+struct Course_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
