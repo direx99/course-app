cdc648dfa2c02e4ab139d246d31adeea14f1df24
Dinith <codse202f060@student.nibm.lk>
2023-06-09 09:26:38 +0530

    review component added

--- View/CourseDetailView.swift
+++ View/CourseDetailView.swift
@@ -8,115 +8,149 @@
 import SwiftUI
 
 struct CourseDetailView: View {
+    init() {
+            // Use this if you want to customize the navigation bar appearance
+            UINavigationBar.appearance().backgroundColor = UIColor(named: "BlackCard")
+            
+            // Change the status bar color to white
+        UIApplication.shared.statusBarStyle = .darkContent
+        }
     var body: some View {
-        ScrollView{
-
         VStack{
-                HStack{
-                    VStack(alignment: .leading){
-                        Text("React - The Complete ")
-                            .font(.system(size: 32))
-                            .fontWeight(.semibold)
-                            .foregroundColor(.white)
-                        Text("Guid")
-                            .font(.system(size: 32))
-                            .fontWeight(.semibold)
-                            .foregroundColor(.white)
-                            .padding(.bottom,1)
-                        
-                        Text("Developement")
-                            .font(.system(size: 20))
-                            .foregroundColor(.gray)
-                        
-                        HStack(spacing:0){
-                            Group{
-                                Image(systemName: "star.fill")
-                                Image(systemName: "star.fill")
-                                Image(systemName: "star.fill")
-                                Image(systemName: "star.fill")
-                                Image(systemName: "star.fill")
+            ScrollView{
+                
+                VStack{
+                    HStack{
+                        VStack(alignment: .leading){
+                            Text("React - The Complete ")
+                                .font(.system(size: 32))
+                                .fontWeight(.semibold)
+                                .foregroundColor(.white)
+                            Text("Guid")
+                                .font(.system(size: 32))
+                                .fontWeight(.semibold)
+                                .foregroundColor(.white)
+                                .padding(.bottom,1)
+                            
+                            Text("Developement")
+                                .font(.system(size: 20))
+                                .foregroundColor(.gray)
+                            
+                            HStack(spacing:0){
+                                Group{
+                                    Image(systemName: "star.fill")
+                                    Image(systemName: "star.fill")
+                                    Image(systemName: "star.fill")
+                                    Image(systemName: "star.fill")
+                                    Image(systemName: "star.fill")
+                                }
+                                .font(.system(size: 12))
+                                
+                                Text("5")
+                                    .foregroundColor(.white)
+                                    .padding(.leading,5)
+                                    .font(.system(size: 12))
+                                Spacer()
+                                Image(systemName: "person.2.fill")
+                                Text("23 234")
+                                    .foregroundColor(.white)
+                                    .padding(.leading,5)
+                                    .font(.system(size: 12))
+                                Spacer()
+                                Image(systemName: "person.2.fill")
+                                Text("23 234")
+                                    .foregroundColor(.white)
+                                    .padding(.leading,5)
+                                    .font(.system(size: 12))
+                                
+                                
                             }
-                            .font(.system(size: 12))
-                            
-                            Text("5")
+                            .foregroundColor(.orange)
+                            .padding(.top,10)
+                        }
+                        
+                        
+                        
+                        Spacer()
+                    }
+                    HStack(){
+                        Text("$19.99")
+                            .foregroundColor(.white)
+                            .font(.system(size: 35))
+                            .fontWeight(.semibold)
+                            .padding(.top,20)
+                        
+                        Spacer()
+                        
+                    }
+                    HStack(){
+                        VStack(alignment:.leading){
+                            Text("About Course")
                                 .foregroundColor(.white)
-                                .padding(.leading,5)
-                                .font(.system(size: 12))
-                            Spacer()
-                            Image(systemName: "person.2.fill")
-                            Text("23 234")
+                                .font(.system(size: 18))
+                                .padding(.top,10)
+                                .padding(.bottom,2)
+                            Text("The React. JS Course Curriculum contains basic elements like introduction, JSX overview, Environmental setup, real-time applications, forms and UI, Component Lifecycle, Event Handling.")
                                 .foregroundColor(.white)
-                                .padding(.leading,5)
                                 .font(.system(size: 12))
-                            Spacer()
-                            Image(systemName: "person.2.fill")
-                            Text("23 234")
-                                .foregroundColor(.white)
-                                .padding(.leading,5)
-                                .font(.system(size: 12))
-                            
+                                .opacity(0.8)
                             
                         }
-                        .foregroundColor(.orange)
-                        .padding(.top,10)
-                    }
-                    
-                    
-                    
-                    Spacer()
-                }
-                HStack(){
-                    Text("$19.99")
-                        .foregroundColor(.white)
-                        .font(.system(size: 35))
-                        .fontWeight(.semibold)
-                        .padding(.top,20)
-                    
-                    Spacer()
-                    
-                }
-                HStack(){
-                    VStack(alignment:.leading){
-                        Text("About Course")
-                            .foregroundColor(.white)
-                            .font(.system(size: 18))
-                            .padding(.top,10)
-                            .padding(.bottom,2)
-                        Text("The React. JS Course Curriculum contains basic elements like introduction, JSX overview, Environmental setup, real-time applications, forms and UI, Component Lifecycle, Event Handling.")
-                            .foregroundColor(.white)
-                            .font(.system(size: 12))
+                        
+                        
+                        
+                        Spacer()
                         
                     }
                     
                     
+                   
                     
-                    Spacer()
+                    
+                    Test()
+                        .padding(.top,-50)
+                    
+                    
                     
                 }
-                
-                
-                
-                
-                
-                    Test()
-                
-               
+                .padding()
+                .padding(.top,40)
+                .frame(maxWidth: .infinity,maxHeight: .infinity)
+                .background(Color("BlackCard"))
+                .padding(.bottom,-25)
                 
             }
-        .padding()
-        .padding(.top,20)
-        .frame(maxWidth: .infinity,maxHeight: .infinity)
-        .background(Color("BlackCard"))
-        .padding(.bottom,-25)
+            
+            .navigationBarTitleDisplayMode(.inline)
+            .background(Color("BlackCard"))
+            .navigationBarHidden(true)
+            .ignoresSafeArea()
+            
+            .navigationViewStyle(StackNavigationViewStyle())
+            VStack{
+              Text("Buy now")
+                    .font(.system(size: 20))
+                    .foregroundColor(.white)
+                    .fontWeight(.semibold)
+                    .frame(width: UIScreen.main.bounds.width * 1 - 50, height: 60)
+                    .opacity(0.9)
 
                 
+            }
+            .background(Color("Orange"))
+            .cornerRadius(32)
+            
         }
+        
+        .padding(.top)
         .background(Color("BlackCard"))
-
-
+       
+        
+        
 
         
     }
+    
     
 }
 
@@ -147,11 +181,18 @@ struct Lesson : View {
                     .padding(.leading,10)
                     .font(.system(size: 18))
                     .fontWeight(.semibold)
-
-                Text("13 Miniutes")
-                    .foregroundColor(.gray)
-                    .padding(.leading,10)
-                    .font(.system(size: 13))
+                HStack{
+                    Text("13 Lectures")
+                        .foregroundColor(.orange)
+                        .padding(.leading,10)
+                        .font(.system(size: 13))
+                    Spacer()
+                    Text("13 min")
+                        .foregroundColor(.gray)
+                        .padding(.trailing,20)
+                        .font(.system(size: 13))
+                    
+                }
             
             
             Spacer()
@@ -173,6 +214,8 @@ struct Test: View {
                 
                 TabView(selection: $selectedTab) {
                     VStack{
+                        VStack{
+
                         HStack{
                             Text("Lessons")
                                 .foregroundColor(.white)
@@ -188,54 +231,72 @@ struct Test: View {
                             Spacer()
                         }
                         .padding(.top)
-                            Lesson()
-                            Lesson()
-                            Lesson()
-                            Lesson()
-                        Lesson()
-                        Lesson()
-                        Lesson()
-                        Lesson()
+                                VStack{
+                                    Group{
+                                        Lesson()
+                                        Lesson()
+                                        Lesson()
+                                        Lesson()
+                                        Lesson()
+                                        Lesson()
+                                        Lesson()
+                                        Lesson()
+                                        Lesson()
+                                        
+                                    }
+
+                                
+                                    Spacer()
+                                }
+                            
+                                .frame(height: 1000)
+                                }
+
                         
                     }
                    
-                        .tabItem {
-                            Label("Tab 1", systemImage: "1.circle")
-                        }
+                        
                         .tag(0)
                     
                     VStack{
-                        HStack{
-                            Text("Lessons")
-                                .foregroundColor(.gray)
-                                .font(.system(size: 20))
-                                .padding(.top,20)
-                                .padding(.bottom,10)
+                        VStack{
+                            HStack{
+                                Text("Lessons")
+                                    .foregroundColor(.gray)
+                                    .font(.system(size: 20))
+                                    .padding(.top,20)
+                                    .padding(.bottom,10)
                                 
-                            Text("Reviews")
-                                .foregroundColor(.white)
-                                .font(.system(size: 20))
-                                .padding(.top,20)
-                                .padding(.bottom,10)
-                                .padding(.leading)
+                                Text("Reviews")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 20))
+                                    .padding(.top,20)
+                                    .padding(.bottom,10)
+                                    .padding(.leading)
+                                
+                                
+                                Spacer()
+                            }
+                            .padding(.top)
+                            VStack{
+                                Review()
+                                Review()
+                                Review()
 
-                                
-                            Spacer()
+                                Review()
+                                    Review()
+                                    Review()
+                                    Review()
+
+                                    Review()
+                                Spacer()
+                            }
+                            .frame(height: 1000)
                         }
-                        .padding(.top)
-                            Lesson()
-                            Lesson()
-                            Lesson()
-                            Lesson()
-                        Lesson()
-                        Lesson()
-                        Lesson()
-                        Lesson()
                         
+
                     }
-                        .tabItem {
-                            Label("Tab 2", systemImage: "2.circle")
-                        }
+                        
                         .tag(1)
                     
                   
@@ -251,10 +312,77 @@ struct Test: View {
                             }
                         }
                 )
-                Spacer()
             }
-            .frame( height: UIScreen.main.bounds.width * 2)
+            .frame( height: UIScreen.main.bounds.width * 3)
 
                     
         }
     }
+
+
+struct Review : View {
+    var body: some View{
+        HStack(alignment: .top){
+            
+          
+                Image("user")
+                .resizable()
+                .scaledToFit()
+            
+            .frame(width: 50,height: 50)
+            .background(Color("BlueRound"))
+            .cornerRadius(50)
+            .padding(.leading)
+            VStack(alignment:.leading,spacing: 2){
+                HStack(){
+                    Text("Amashi Silva")
+                        .foregroundColor(.white)
+                        .padding(.leading,10)
+                        .font(.system(size: 18))
+                        .fontWeight(.semibold)
+                    Spacer()
+                    Text("2 months ago")
+                        .foregroundColor(.gray)
+                        .padding(.trailing,20)
+                        .font(.system(size: 13))
+                    
+                }
+                
+               
+                HStack(spacing:0){
+                    Group{
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                    }
+                    .font(.system(size: 12))
+                }
+                .foregroundColor(.orange)
+                .padding(.leading,10)
+                
+                VStack{
+                    Text("I recently completed a comprehensive React course, and I must say it exceeded all my expectations. This course was an absolute game-changer for my development journey. From start to finish, it provided a solid foundation in React and equipped me with the necessary skills to build complex web applications with confidence.")
+                        .font(.system(size: 12))
+                        .foregroundColor(.white)
+                        .padding(.leading,10)
+                        
+
+                }
+                .frame(height: 70)
+                
+                
+                   
+                    
+                
+            
+            
+        }
+            Spacer()
+            
+
+        }
+        
+    }
+}
